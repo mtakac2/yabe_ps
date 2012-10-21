8b9e6d3a1afc29952db8984698f1c89227fc593a
Marek Takac <takacmarek1221@gmail.com>
2012-10-21 21:28:06 +0200

    Created Comment model

--- app/models/Post.java
+++ app/models/Post.java
@@ -3,6 +3,8 @@ package models;
 import java.util.*;
 import javax.persistence.*;
 
+import org.hibernate.annotations.Type;
+
 import play.db.jpa.*;
 
 @Entity
@@ -10,15 +12,27 @@ import play.db.jpa.*;
 public class Post extends Model {
 	
 	public String title;
-	public Date postedAt;	
+	public Date postedAt;
+	@Lob
+	@Type(type = "org.hibernate.type.TextType")
 	public String content;
 	@ManyToOne
 	public User author;
+	@OneToMany(mappedBy="post", cascade=CascadeType.ALL)
+	public List<Comment> comments;
 	
 	public Post(User author, String title, String content) {
+		this.comments = new ArrayList<Comment>();
 		this.author = author;
 		this.title = title;
 		this.content = content;
 		this.postedAt = new Date();
+	}
+	
+	public Post addComment(String author, String content) {
+		Comment newComment = new Comment(this, author, content).save();
+		this.comments.add(newComment);
+		this.save();
+		return this;
 	}
 }
